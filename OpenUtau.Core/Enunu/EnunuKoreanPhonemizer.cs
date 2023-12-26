ff82d39fc9dd589abb24c802d0cfe17f7ea3ec16
나현 김 <inthe_6788@naver.com>
2023-12-26 12:41:29 +0900

    Add EnunuKoreanPhonemizer

---
+++ OpenUtau.Core/Enunu/EnunuKoreanPhonemizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using K4os.Hash.xxHash;
+using OpenUtau.Api;
+using OpenUtau.Core.Editing;
+using OpenUtau.Core.Ustx;
+using OpenUtau.Core.G2p;
+using Serilog;
+
+namespace OpenUtau.Core.Enunu {
+    [Phonemizer("Enunu Korean Phonemizer", "ENUNU KO", "EX3", language:"KO")]
+    public class EnunuKoreanPhonemizer : EnunuPhonemizer {
+        readonly string PhonemizerType = "ENUNU KO";
+
+    }
+}
