85a73162bdfdfa2fddd729286c47e4df967c9302
나현 김 <inthe_6788@naver.com>
2023-12-28 00:59:26 +0900

    fixed minor typo

--- OpenUtau.Core/Enunu/EnunuKoreanPhonemizer.cs
+++ OpenUtau.Core/Enunu/EnunuKoreanPhonemizer.cs
@@ -5,11 +5,7 @@ using System.Linq;
 using System.Text;
 using K4os.Hash.xxHash;
 using OpenUtau.Api;
-using OpenUtau.Core.Editing;
 using OpenUtau.Core.Ustx;
-using OpenUtau.Core.G2p;
-using Serilog;
-using System.Collections;
 
 namespace OpenUtau.Core.Enunu {
     [Phonemizer("Enunu Korean Phonemizer", "ENUNU KO", "EX3", language:"KO")]
